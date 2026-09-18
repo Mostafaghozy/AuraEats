08d4bb585a5a83755d24288a433bbd893d3906d4
Mostafa <your_email@example.com>
2026-09-18 18:41:54 +0300

    improve nav bar to Liquid Glass

--- lib/root.dart
+++ lib/root.dart
@@ -1,8 +1,11 @@
+import 'dart:ui';
+
 import 'package:aura_eats/core/constants/app_colors.dart';
 import 'package:aura_eats/features/auth/views/profile_view.dart';
 import 'package:aura_eats/features/cart/views/cart_view.dart';
 import 'package:aura_eats/features/home/views/home_view.dart';
 import 'package:aura_eats/features/orderHistory/views/order_history_view.dart';
+import 'package:aura_eats/widgets/custom_bottom_nav_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -15,63 +18,102 @@ class Root extends StatefulWidget {
 
 class _RootState extends State<Root> {
   late PageController controller;
+
   late List<Widget> screens;
+
   int currentScreen = 0;
+
   @override
   void initState() {
-    controller = PageController(initialPage: currentScreen);
-    screens = [HomeView(), CartView(), OrderHistoryView(), ProfileView()];
     super.initState();
+
+    controller = PageController(initialPage: currentScreen);
+
+    screens = [HomeView(), CartView(), OrderHistoryView(), ProfileView()];
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void changeScreen(int index) {
+    setState(() {
+      currentScreen = index;
+    });
+
+    controller.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 150),
+      curve: Curves.linear,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: false, //to show bottom navigation bar
+
       body: PageView(
         controller: controller,
-        physics: NeverScrollableScrollPhysics(),
+        physics: const NeverScrollableScrollPhysics(),
         children: screens,
       ),
 
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: AppColors.primary,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(30),
-            topLeft: Radius.circular(30),
-          ),
-        ),
-        child: BottomNavigationBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.grey.shade500.withOpacity(0.7),
-          currentIndex: currentScreen,
-          onTap: (index) {
-            setState(() => currentScreen = index);
-            controller.jumpToPage(currentScreen);
-          },
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
 
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home),
-              label: "home",
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+
+            child: Container(
+              height: 80,
+
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.18),
+
+                borderRadius: BorderRadius.circular(30),
+
+                border: Border.all(color: AppColors.primary, width: 1),
+              ),
+
+              child: Row(
+                children: [
+                  CustomBottomNavItem(
+                    index: 0,
+                    icon: CupertinoIcons.home,
+                    label: 'Home',
+                    isSelected: currentScreen == 0,
+                    onTap: () => changeScreen(0),
+                  ),
+                  CustomBottomNavItem(
+                    index: 1,
+                    icon: CupertinoIcons.cart,
+                    label: 'Cart',
+                    isSelected: currentScreen == 1,
+                    onTap: () => changeScreen(1),
+                  ),
+                  CustomBottomNavItem(
+                    index: 2,
+                    icon: Icons.local_restaurant_sharp,
+                    label: 'Orders',
+                    isSelected: currentScreen == 2,
+                    onTap: () => changeScreen(2),
+                  ),
+                  CustomBottomNavItem(
+                    index: 3,
+                    icon: CupertinoIcons.profile_circled,
+                    label: 'Profile',
+                    isSelected: currentScreen == 3,
+                    onTap: () => changeScreen(3),
+                  ),
+                ],
+              ),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.cart),
-              label: "Cart",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.local_restaurant_sharp),
-              label: "Order History",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.profile_circled),
-              label: "Profile",
-            ),
-          ],
+          ),
         ),
       ),
     );
